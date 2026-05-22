24f6e7f8c7861593e95fb8a319ca59fa8ec5d566
RightTr <zhoutianhao314@whu.edu.cn>
2026-05-22 17:56:43 +0800

    NUS thermal dataset test on IMU stereo mode

--- vins_estimator/src/nus_thermal_publisher.cpp
+++ vins_estimator/src/nus_thermal_publisher.cpp
@@ -3,7 +3,9 @@
 #include <cerrno>
 #include <cstdlib>
 #include <dirent.h>
+#include <fstream>
 #include <limits>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -13,6 +15,7 @@
 #include <opencv2/imgproc.hpp>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/Imu.h>
 
 namespace
 {
@@ -20,6 +23,17 @@ struct Frame
 {
     std::string path;
     double stamp = 0.0;
+};
+
+struct ImuSample
+{
+    double stamp = 0.0;
+    double gx = 0.0;
+    double gy = 0.0;
+    double gz = 0.0;
+    double ax = 0.0;
+    double ay = 0.0;
+    double az = 0.0;
 };
 
 struct Rectifier
@@ -69,6 +83,14 @@ bool parseStamp(const std::string &name, double &stamp)
     return true;
 }
 
+bool parseImuSample(const std::string &line, ImuSample &sample)
+{
+    std::istringstream ss(line);
+    return static_cast<bool>(ss >> sample.stamp
+                                >> sample.gx >> sample.gy >> sample.gz
+                                >> sample.ax >> sample.ay >> sample.az);
+}
+
 std::vector<Frame> loadFrames(const std::string &directory)
 {
     std::vector<Frame> frames;
@@ -99,6 +121,46 @@ std::vector<Frame> loadFrames(const std::string &directory)
         return a.stamp < b.stamp;
     });
     return frames;
+}
+
+std::vector<ImuSample> loadImuSamples(const std::string &path)
+{
+    std::vector<ImuSample> samples;
+    std::ifstream input(path);
+    if (!input.is_open())
+    {
+        ROS_ERROR_STREAM("Failed to open IMU file: " << path);
+        return samples;
+    }
+
+    std::string line;
+    while (std::getline(input, line))
+    {
+        if (line.empty())
+            continue;
+        ImuSample sample;
+        if (!parseImuSample(line, sample))
+            continue;
+        samples.push_back(sample);
+    }
+
+    std::sort(samples.begin(), samples.end(), [](const ImuSample &a, const ImuSample &b) {
+        return a.stamp < b.stamp;
+    });
+    return samples;
+}
+
+size_t firstImuIndexForImageTime(const std::vector<ImuSample> &samples, double image_time)
+{
+    if (samples.empty())
+        return 0;
+    auto it = std::lower_bound(samples.begin(), samples.end(), image_time,
+                               [](const ImuSample &sample, double stamp) {
+                                   return sample.stamp < stamp;
+                               });
+    if (it == samples.begin())
+        return 0;
+    return static_cast<size_t>((it - samples.begin()) - 1);
 }
 
 Rectifier makeNusThermalRectifier(bool enabled, double alpha)
@@ -169,6 +231,20 @@ sensor_msgs::ImagePtr readMonoImage(const std::string &path,
     header.frame_id = frame_id;
     return cv_bridge::CvImage(header, sensor_msgs::image_encodings::MONO8, image).toImageMsg();
 }
+
+sensor_msgs::Imu makeImuMsg(const ImuSample &sample, const std::string &frame_id)
+{
+    sensor_msgs::Imu msg;
+    msg.header.stamp = ros::Time().fromSec(sample.stamp);
+    msg.header.frame_id = frame_id;
+    msg.angular_velocity.x = sample.gx;
+    msg.angular_velocity.y = sample.gy;
+    msg.angular_velocity.z = sample.gz;
+    msg.linear_acceleration.x = sample.ax;
+    msg.linear_acceleration.y = sample.ay;
+    msg.linear_acceleration.z = sample.az;
+    return msg;
+}
 } // namespace
 
 int main(int argc, char **argv)
@@ -182,27 +258,35 @@ int main(int argc, char **argv)
     std::string right_dir;
     std::string image0_topic;
     std::string image1_topic;
+    std::string imu_file;
+    std::string imu_topic;
     std::string left_frame_id;
     std::string right_frame_id;
+    std::string imu_frame_id;
     double playback_rate = 1.0;
     int start_index = 0;
     int max_frames = 0;
     bool loop = false;
     bool rectify_images = true;
+    bool publish_imu = false;
     double rectify_alpha = 0.0;
 
     pnh.param<std::string>("data_root", data_root, "/mnt/Data/homebrew_thermal/Nus/run_20250912_180927");
     pnh.param<std::string>("left_dir", left_dir, "left_thermal/left_motion");
     pnh.param<std::string>("right_dir", right_dir, "right_thermal/right_motion");
+    pnh.param<std::string>("imu_file", imu_file, "realsense/imu/imu_synced.txt");
     pnh.param<std::string>("image0_topic", image0_topic, "/cam0/image_raw");
     pnh.param<std::string>("image1_topic", image1_topic, "/cam1/image_raw");
+    pnh.param<std::string>("imu_topic", imu_topic, "/imu0");
     pnh.param<std::string>("left_frame_id", left_frame_id, "nus_thermal_left");
     pnh.param<std::string>("right_frame_id", right_frame_id, "nus_thermal_right");
+    pnh.param<std::string>("imu_frame_id", imu_frame_id, "realsense_imu");
     pnh.param("playback_rate", playback_rate, 1.0);
     pnh.param("start_index", start_index, 0);
     pnh.param("max_frames", max_frames, 0);
     pnh.param("loop", loop, false);
     pnh.param("rectify_images", rectify_images, true);
+    pnh.param("publish_imu", publish_imu, false);
     pnh.param("rectify_alpha", rectify_alpha, 0.0);
 
     if (playback_rate <= 0.0)
@@ -218,11 +302,18 @@ int main(int argc, char **argv)
 
     const std::string left_path = left_dir.empty() || left_dir[0] == '/' ? left_dir : joinPath(data_root, left_dir);
     const std::string right_path = right_dir.empty() || right_dir[0] == '/' ? right_dir : joinPath(data_root, right_dir);
+    const std::string imu_path = imu_file.empty() || imu_file[0] == '/' ? imu_file : joinPath(data_root, imu_file);
     const std::vector<Frame> left_frames = loadFrames(left_path);
     const std::vector<Frame> right_frames = loadFrames(right_path);
+    const std::vector<ImuSample> imu_samples = publish_imu ? loadImuSamples(imu_path) : std::vector<ImuSample>();
     if (left_frames.empty() || right_frames.empty())
     {
         ROS_ERROR_STREAM("No frames found. left=" << left_frames.size() << " right=" << right_frames.size());
+        return 1;
+    }
+    if (publish_imu && imu_samples.empty())
+    {
+        ROS_ERROR_STREAM("publish_imu is true but no IMU samples were loaded from " << imu_path);
         return 1;
     }
 
@@ -244,19 +335,37 @@ int main(int argc, char **argv)
 
     ros::Publisher pub_left = nh.advertise<sensor_msgs::Image>(image0_topic, 10);
     ros::Publisher pub_right = nh.advertise<sensor_msgs::Image>(image1_topic, 10);
+    ros::Publisher pub_imu;
+    if (publish_imu)
+        pub_imu = nh.advertise<sensor_msgs::Imu>(imu_topic, 2000);
     const Rectifier rectifier = makeNusThermalRectifier(rectify_images, rectify_alpha);
 
     ROS_INFO_STREAM("Publishing NUS thermal stereo sequence"
                     << " left=" << left_path
                     << " right=" << right_path
+                    << " imu=" << (publish_imu ? imu_path : std::string("disabled"))
                     << " pairs=" << (end_index - static_cast<size_t>(start_index))
                     << " playback_rate=" << playback_rate);
 
     do
     {
+        size_t imu_index = publish_imu ? firstImuIndexForImageTime(imu_samples, left_frames[start_index].stamp) : 0;
         for (size_t i = static_cast<size_t>(start_index); ros::ok() && i < end_index; ++i)
         {
             const ros::Time stamp = ros::Time().fromSec(left_frames[i].stamp);
+            if (publish_imu)
+            {
+                while (imu_index < imu_samples.size() && imu_samples[imu_index].stamp < left_frames[i].stamp)
+                {
+                    pub_imu.publish(makeImuMsg(imu_samples[imu_index], imu_frame_id));
+                    ++imu_index;
+                }
+                if (imu_index < imu_samples.size())
+                {
+                    pub_imu.publish(makeImuMsg(imu_samples[imu_index], imu_frame_id));
+                    ++imu_index;
+                }
+            }
             sensor_msgs::ImagePtr left_msg = readMonoImage(left_frames[i].path, stamp, left_frame_id, rectifier, true);
             sensor_msgs::ImagePtr right_msg = readMonoImage(right_frames[i].path, stamp, right_frame_id, rectifier, false);
             if (!left_msg || !right_msg)
